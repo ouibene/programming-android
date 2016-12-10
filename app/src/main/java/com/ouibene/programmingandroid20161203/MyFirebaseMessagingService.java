623cd9a23779b8c7fa55415b5d6b3b11db2af6d7
LYE <ouibene@gmail.com>
2016-12-10 12:58:39 +0900

    Revert "MyFirebaseInstanceIdServeice 구현을 통해 Push Token Refresh 콜백을 받음"
    This reverts commit a5d1836c6df5970176a21f5ce4b489b1064d6e64.

--- app/src/main/java/com/ouibene/programmingandroid20161203/MyFirebaseMessagingService.java
+++ app/src/main/java/com/ouibene/programmingandroid20161203/MyFirebaseMessagingService.java
@@ -2,7 +2,6 @@ package com.ouibene.programmingandroid20161203;
 
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,12 +13,8 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    final static String TAG = MyFirebaseMessagingService.class.getName();
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "onMessageReceived" + remoteMessage.getData());
-
         if (remoteMessage.getData().size() > 0) {
             String messageBody = remoteMessage.getData().get("message_body");
             if (messageBody != null) {
@@ -33,13 +28,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
     }
 
-    void toast (final String message) {
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-            }
-        });
-    } //push 구현. notification을 사용하면 background에서는 푸시받을 수 없음
-}
+            void toast (final String message) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                }
+            });
+        } //push 구현. notification을 사용하면 background에서는 푸시받을 수 없음
+    }
